f264dcdf5055e35bff9c217c4ca7ba52443e2b98
ahmad shah <ahmadshah102005@gmail.com>
2025-01-07 03:32:55 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import PrivacyPolicy from "./components/PrivacyPolicy"
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
-   <PrivacyPolicy/>
+   <PrivacyPolicy />
     </div>
-  )
+   )
 }
 
 export default App
